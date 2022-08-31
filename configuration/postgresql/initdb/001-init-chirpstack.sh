da2c1ac5403da0e9ee2f1c75e5705b9a81f84886
Carlos Eduardo Batista <carlos.batista@hvex.com.br>
2022-08-31 17:52:10 -0300

    refact:alterado dados do arquivo docker yml

--- configuration/postgresql/initdb/001-init-chirpstack.sh
+++ configuration/postgresql/initdb/001-init-chirpstack.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 set -e
-
+# pesquisar if not exists
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    create role chirpstack with login password 'chirpstack';
-    create database chirpstack with owner chirpstack;
+    create role chirpstack_as with login password 'dbpassword';
+    create role chirpstack_ns with login password 'dbpassword';
+
+    create database chirpstack_as with owner chirpstack_as;
+    create database chirpstack_ns with owner chirpstack_ns;
+
+    CREATE USER med WITH PASSWORD 'pass';
+    ALTER USER med WITH SUPERUSER;
+    \du
+    CREATE DATABASE medicoes WITH OWNER med;
+    \q
+
 EOSQL
